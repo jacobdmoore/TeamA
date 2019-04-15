2f1d325bb14455e0a3cddd007d632b026fb2c193
nathanielclark6 <47364160+nathanielclark6@users.noreply.github.com>
2019-04-15 10:51:55 -0500

    Add files via upload
    opens ppt and allows you to change each notes page in a file

--- finalproject.py
+++ finalproject.py
@@ -1,18 +1,24 @@
 from pptx import Presentation
 prsname=raw_input("Enter Prestation name to modify: ")
 prs=Presentation(prsname)
-#numslides=len(prs.slides) # produces number of slides in deck
-first_slide=prs.slides[0]
+numslides=len(prs.slides) # produces number of slides in deck
+slidenum=0
 
-print(first_slide.has_notes_slide)
-#slide.has_notes_slide #checks to see if slide has notes slide
+while slidenum<numslides:
 
-notes_slide=first_slide.notes_slide
-
-#notes_slide=slide.notes_slide : acceses nots slide
-text_frame=notes_slide.notes_text_frame
-text_frame.text="this is slide 1 in the notes section"
+    current_slide=prs.slides[slidenum]
+    notes_slide=current_slide.notes_slide
+    #notes_slide=slide.notes_slide : acceses nots slide
+    text_frame=notes_slide.notes_text_frame
+    print('Current Slide'+str(slidenum+1))
+    newtext=raw_input("Enter text for slide here")
+    text_frame.text=text_frame.text+newtext
+    slidenum=slidenum+1
 
 newprs=prsname.split('.') #removes pptx fromname
 new=newprs[0]
 prs.save(new+' with notes.pptx') #adds withnotes.pptx to name
+
+
+#print(first_slide.has_notes_slide)
+#slide.has_notes_slide #checks to see if slide has notes slide
