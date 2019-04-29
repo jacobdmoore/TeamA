4cfa360c27c526dc6ae15eded46e5046816d3232
jacobdmoore <47227108+jacobdmoore@users.noreply.github.com>
2019-04-29 10:40:52 -0500

    full control from shell

--- mainlistener.sh
+++ mainlistener.sh
@@ -1,6 +1,23 @@
-# Allows two pythons scripts to run simultaneously.
-
 # This is the script to be run by the lecturer before the lecture.
+
+# Shell script asks user question, passes answer to Python.
+read -r -p "Will you be using Google Slides to present today? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        google_slides=1
+        ;;
+    *)
+        google_slides=0
+        ;;
+esac
+
+# Allows two pythons scripts to run simultaneously.
 # First program records audio, second one records keystrokes and outputs as action_key_tracker.txt.
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
 python recordUI.py &
-python keylistener.py &
+python keylistener.py "$google_slides" &
+echo 'Now recording! To cease recording, press CTRL + C.' &
+
+wait
