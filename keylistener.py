f2ccbfbeca3582e499ee0148a3a767368c05b130
jacobdmoore <47227108+jacobdmoore@users.noreply.github.com>
2019-04-29 10:41:12 -0500

    listens to shell

--- keylistener.py
+++ keylistener.py
@@ -8,32 +8,20 @@ from pynput.keyboard import Key
 import logging
 import datetime
 import time
+import sys
+import signal
+
+google = 0
+# Get input from shell script.
+google = sys.argv[1]
+
+print('IWH')
     
 # MAKE CTRL + F5 an option to go to current slide, CTRL + SHIFT + F5 goes to beginning
 # CTRL SHIFT LEFT/RIGHT to switch desktops
 # ALT TAB
 
 start_time = time.time()
-
-# Prompt user to specify whether they will be using Google Slides
-yes = ['yes','Yes','y','Y']
-no = ['no','No','n','N']
-cancel = ['cancel','Cancel','c','C']
-valid_input = False
-prompt = '\nWill you be using Google Slides to present today? (y/n)\n'
-google = input(prompt)
-
-while not valid_input:
-    if google in yes:
-        google = True
-        valid_input = 1
-    elif google in no:
-        google = False
-        valid_input = 1
-    elif google in cancel:
-        raise SystemExit(0)
-    else:
-        google = input("\nInvalid response. Please respond with 'y' or 'n'. You may cancel by responding 'c'. \n" + prompt)
 
 # Log of all keystrokes.
 logging.basicConfig(filename=("full_keylog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
@@ -65,27 +53,27 @@ def on_press(key):
     # The cases are pretty self-explanatory. Shift and F5 are tracked as the 
     # ways the slideshow is restarted after it is exited with Esc.
     if str(key) in next_keys:
-        print('NEXT with',key,'at ',timestamp)
+        #print('NEXT with',key,'at ',timestamp)
         with open('action_key_tracker.txt', 'a') as file:
             file.write('\nNEXT with ' + str(key) + ' at ' + str(timestamp))
     elif str(key) in prev_keys:
-        print('PREVIOUS with',key,'at ',timestamp)
+        #print('PREVIOUS with',key,'at ',timestamp)
         with open('action_key_tracker.txt', 'a') as file:
             file.write('\nPREVIOUS with ' + str(key) + ' at ' + str(timestamp))
     elif str(key) == 'Key.esc':
-        print('EXIT with Esc at ',timestamp)
+        #print('EXIT with Esc at ',timestamp)
         with open('action_key_tracker.txt', 'a') as file:
             file.write('\nEXIT with Esc at ' + str(timestamp))
     elif str(key) in ['Key.ctrl_r','Key.ctrl']:
-        print('CTRL KEY at ',timestamp)
+        #print('CTRL KEY at ',timestamp)
         with open('action_key_tracker.txt', 'a') as file:
             file.write('\nCTRL with ' + str(key) + ' at ' + str(timestamp))
     elif str(key) in ['Key.shift_r','Key.shift']:
-        print('SHIFT KEY at ',timestamp)
+        #print('SHIFT KEY at ',timestamp)
         with open('action_key_tracker.txt', 'a') as file:
             file.write('\nSHIFT with ' + str(key) + ' at ' + str(timestamp))
     elif str(key) == 'Key.f5':
-        print('ENTER with F5 at ',timestamp)
+        #print('ENTER with F5 at ',timestamp)
         with open('action_key_tracker.txt', 'a') as file:
             file.write('\nENTER with F5 at ' + str(timestamp))
 
@@ -105,7 +93,7 @@ def on_click(x, y, button, pressed):
     
     # Only the left mouse click advances slides.
     if str(button) == 'Button.left':
-        print('NEXT with left click at ',timestamp)
+        #print('NEXT with left click at ',timestamp)
         with open('action_key_tracker.txt', 'a') as file:
             file.write('\nNEXT with left click at ' + str(timestamp))
 
@@ -119,15 +107,16 @@ def on_scroll(x, y, dx, dy):
     # -1 is equivalent to a downscroll with the mouse.
     # 1 is an upscroll.
     if dy == -1:
-        print('NEXT with down scroll at ',timestamp)
+        #print('NEXT with down scroll at ',timestamp)
         with open('action_key_tracker.txt', 'a') as file:
             file.write('\nNEXT with down scroll at ' + str(timestamp))
     elif dy == 1:
-        print('NEXT with up scroll',timestamp)
+        #print('NEXT with up scroll',timestamp)
         with open('action_key_tracker.txt', 'a') as file:
             file.write('\nPREVIOUS with up scroll at ' + str(timestamp))
 
-# Listen to both mouse (click and scroll) and keyboard actions.
+
+ # Listen to both mouse (click and scroll) and keyboard actions.
 with MouseListener(on_click=on_click, on_scroll=on_scroll) as listener:
     with KeyboardListener(on_press=on_press) as listener:
         listener.join()
