e58327b329a4abb48a9d28877476621f31d89cae
jacobdmoore <47227108+jacobdmoore@users.noreply.github.com>
2019-05-01 13:49:49 -0500

    added initialization

--- mainlistener.sh
+++ mainlistener.sh
@@ -1,5 +1,27 @@
 # This is the script to be run by the lecturer before the lecture.
 
+# Shell script asks user if it can initialize, and then it creates the subfolders/files necessary.
+read -r -p "Before recording, it is best for me to initialize the appropriate files and folders. This could result in old transcribed data being deleted accidentally. Proceed? [y/N]" response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        rm action_key_tracker.txt
+        rm filenumber.txt
+        rm full_keylog.txt
+        rm timestamp_history.txt
+        mkdir -p ./slideVoice
+        mkdir -p ./tempFiles
+        mkdir -p ./textFiles
+        mkdir -p ./voiceFiles
+        rm ./slideVoice/*
+        rm ./tempFiles/*
+        rm ./textFiles/*
+        rm ./voiceFiles/*
+        ;;
+    *)
+        exit 0
+        ;;
+esac
+
 # Shell script asks user question, passes answer to Python.
 read -r -p "Will you be using Google Slides to present today? [y/N] " response
 case "$response" in
