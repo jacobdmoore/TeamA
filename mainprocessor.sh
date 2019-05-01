701ec1e19b71142cfba14a35bf6818e213b0b168
nathanielclark6 <47364160+nathanielclark6@users.noreply.github.com>
2019-04-30 23:15:15 -0500

    Added pptx portion and updated mainproc
    Needed to update mainprocessor to add a file counter into the bash script. This will only work if Varun deletes files in textFiles every time. I think we should delete them as part of the first shell.

--- mainprocessor.sh
+++ mainprocessor.sh
@@ -5,5 +5,13 @@ trap "kill 0" EXIT
 echo 'Processing transcribed audio and keyboad/mouse actions...'
 python actionparser.py
 python recordSplit.py
+
+### added code
+cd textFiles
+ls -f . | wc -l>filenumber.txt  ### counts number of text files in textFiles and outputs it to a text file. number of files is # in text file minus 3
+cd ..
+mv textFiles/filenumber.txt filenumber.txt  ##moves filenumber.txt to parent directory 
+
 python populatenotes.py
+
 echo 'Complete!'
