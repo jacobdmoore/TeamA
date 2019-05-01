b4e7ab7c07eaf6d6c98c046743f8247bb9d769e5
jacobdmoore <47227108+jacobdmoore@users.noreply.github.com>
2019-05-01 13:22:28 -0500

    ability to read 'end' in timestamps

--- recordSplit.py
+++ recordSplit.py
@@ -11,6 +11,7 @@ def recordSplit(a=75,b=165,slide=2):
     fileName= os.getcwd() + "/slideVoice/"  + str(slide) + ".wav"
     reqVoice.export(fileName, format="wav")
     splitSlideVoice(fileName,slide)
+    print('completed slide',slide)
 
 with open('timestamp_history.txt','r') as file:
     f = file.read()
@@ -20,12 +21,17 @@ lines_list.pop(0)
 
 timestamp_list = []
 
+file_length = len(AudioSegment.from_file(os.getcwd() + "/voiceFiles/lecture.wav"))/1000
+
 for line in lines_list:
     if line: #excludes any empty lines that might arise
         split_line = line.split(' ') #split up line by spaces
         start = split_line[-3] #start time is always 2nd to last word
         end = split_line[-1] #end time is always last word
+        if end == 'end':
+            end = file_length
         slide = split_line[1][:-1] #excludes colon
+        timestamp_list.append((slide,start,end))
 
 for slide, start, end in timestamp_list:
-    recordSplit(start,end,slide)
+    recordSplit(float(start),float(end),slide)
