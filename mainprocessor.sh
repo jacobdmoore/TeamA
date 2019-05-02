d0461eef47d6fa35ca469f14952b9bc619140a67
jacobdmoore <47227108+jacobdmoore@users.noreply.github.com>
2019-05-02 14:59:35 -0500

    Update mainprocessor.sh

--- mainprocessor.sh
+++ mainprocessor.sh
@@ -6,12 +6,6 @@ echo 'Processing transcribed audio and keyboad/mouse actions...'
 python actionparser.py
 python recordSplit.py
 
-### added code
-cd textFiles
-ls -f . | wc -l>filenumber.txt  ### counts number of text files in textFiles and outputs it to a text file. number of files is # in text file minus 3
-cd ..
-mv textFiles/filenumber.txt filenumber.txt  ##moves filenumber.txt to parent directory 
-
-python populatenotes.py
+python newpopulatenotes.py
 
 echo 'Complete!'
